e07a36e8c224aecd21c8a3b27208a275c05025c6
shawn4tech <shawn.cui.tech@gmail.com>
2019-06-02 16:53:34 -0400

    Added publisher confirms and publisher returns

--- src/main/java/one/shawn/rabbitmq/service/RabbitProducer.java
+++ src/main/java/one/shawn/rabbitmq/service/RabbitProducer.java
@@ -1,5 +1,11 @@
 package one.shawn.rabbitmq.service;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +14,32 @@ import one.shawn.rabbitmq.config.RabbitConfig;
 import one.shawn.rabbitmq.model.RabbitMessage;
 
 @Service
-public class RabbitProducer {
+public class RabbitProducer implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnCallback{
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
+	private Logger logger = LoggerFactory.getLogger(RabbitProducer.class);
+	
+	@PostConstruct
+	private void setRabbitTemplate() {
+		rabbitTemplate.setConfirmCallback(this);
+		rabbitTemplate.setReturnCallback(this);
+	}
 	
 	public void sendMessage(RabbitMessage message) {
 		this.rabbitTemplate.convertAndSend(RabbitConfig.QUEUE_MESSAGES, message);
+	}
+
+	@Override
+	public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+		logger.error("Cannot deliver to the queue. Reply text: {}", replyText);
+	}
+
+	@Override
+	public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+		if (ack) {
+			logger.info("Send to the excchange");
+		} else {
+			logger.error("Cannot deliver to the exchange. Cause: {}", cause);
+		}
 	}
 }
